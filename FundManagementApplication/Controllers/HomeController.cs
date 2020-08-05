22cdba683bf80f0ba2a48489f9fb64f6a354f749
Jason Tiong <JTAW@Jasons-MacBook-Pro-2.local>
2020-08-05 22:19:21 +0800

    updated FactSheet

--- FundManagementApplication/Controllers/HomeController.cs
+++ FundManagementApplication/Controllers/HomeController.cs
@@ -19,36 +19,41 @@ namespace FundManagementApplication.Controllers {
                 }},
                 PerformanceCharts = new PerformanceChartDto() {
                     Date = new List<string>() {
-                        "02-03-1992",
-                        "03-23-1903",
-                        "02-03-2000",
-                        "03-23-2001",
-                        "02-03-2005",
-                        "03-23-2008"
+                        "02-01-2019",
+                        "02-02-2019",
+                        "04-03-2019",
+                        "05-04-2019",
+                        "06-03-2019",
+                        "07-23-2019",
+                        "08-23-2019",
+                        "09-23-2019",
+                        "10-23-2019",
+                        "11-23-2019"
+
                     },
                     Performances = new List<PerformanceDto>() {
                         new PerformanceDto() {
                             Name = "Global",
                             CumulativeReturns = new List<decimal>(){
-                                300, 2000, 30, 1510, 5000, 3000,
+                                300, 2000, 300000, 1510, 5000, 3000,2000,1000,1500,4000
                             }
                         },
                         new PerformanceDto() {
                             Name = "BenchmarkA",
                             CumulativeReturns = new List<decimal>(){
-                                500, 6000, 5550, 60, 250, 8000
+                                500, 6000, 15550, 60, 250, 8000,2000,1000,1500,5000
                             }
                         },
                         new PerformanceDto() {
                             Name = "BenchmarkB",
                             CumulativeReturns = new List<decimal>(){
-                                200, 8700, 2000, 5500, 2005, 87,
+                                200, 8700, 100000, 5500, 2005, 87,2000,1000,1500,7000
                             }
                         },
                         new PerformanceDto() {
                             Name = "BenchmarkC",
                             CumulativeReturns = new List<decimal>(){
-                                200, 1000, 200000, 55000, 20005, 807,
+                                200, 1000, 200000, 55000, 20005, 807,2000,1000,1500,8000
                             }
                         }
                     }
