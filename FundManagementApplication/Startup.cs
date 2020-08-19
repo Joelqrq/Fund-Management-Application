2439e7edb9538ced40a6331b69dffe6674b7b611
JoelQ <joelquey@hotmail.com>
2020-08-19 13:45:48 +0800

    Updated some environment

--- FundManagementApplication/Startup.cs
+++ FundManagementApplication/Startup.cs
@@ -73,6 +73,7 @@ namespace FundManagementApplication {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
